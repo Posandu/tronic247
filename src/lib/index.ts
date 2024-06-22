4328da264348aa877f4c7f924635696eefe8434d
Posandu Mapa <posandumapa@gmail.com>
2024-06-22 14:16:44 +0530

    Fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 export const formattedTitle = (name: string) => `${name} | Tronic247`;
 
-export const SITE_URL = 'https://tronic247.com/';
+export const SITE_URL = 'https://www.tronic247.com/';
